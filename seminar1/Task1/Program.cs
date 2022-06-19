a9380a675162677df53417a2f44d60a7d5b57e04
Константин Могутов <kravchenkoluba@yandex.ru>
2022-06-19 20:36:13 +0300

    correct cod

---
+++ seminar1/Task1/Program.cs
@@ -0,0 +1,18 @@
+﻿int numberA, numberB;
+
+Console.WriteLine("Введите первое число");
+numberA = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число");
+numberB = Convert.ToInt32(Console.ReadLine());
+
+if(numberA > numberB)
+{
+    Console.WriteLine(numberA);
+}
+else
+{
+   Console.WriteLine(numberB);
+} 
+
+
