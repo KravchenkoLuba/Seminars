291b25eecc91ff4629abcc2271e66bca7fea19e9
Константин Могутов <kravchenkoluba@yandex.ru>
2022-06-19 20:52:58 +0300

    add Task_4

---
+++ seminar1/Task_4/Program.cs
@@ -0,0 +1,31 @@
+﻿int N;
+int current;
+
+Console.Write("Input number  ");
+N = Convert.ToInt32(Console.ReadLine());
+
+current = 1;
+
+if(N > 0)
+
+{
+    while(current <= N)
+    {
+        if(current % 2 == 0)
+        {
+            Console.Write(current + " ");
+        }
+        current++;
+    }
+}
+else 
+{
+    while(current >= N)
+    {
+        if(current % 2 == 0)
+        {
+            Console.Write(current + " ");
+        }
+        current--;
+    }
+}
