a9bcd722df56dcdbed4e4311d7c861611df49322
Константин Могутов <kravchenkoluba@yandex.ru>
2022-06-24 23:23:41 +0300

    First commit

---
+++ seminar2/Program.cs
@@ -0,0 +1,38 @@
+﻿int TwoNumbers()
+{
+    int rand = new Random().Next(100, 1000);
+    Console.WriteLine("Our current number is " + rand);
+    int sot = rand/100;
+    int ed = rand % 10;
+    int result = sot*10 + ed*1;
+    return result;
+}
+/*
+int a = TwoNumbers();
+Console.WriteLine("result number is " + a);
+*/
+
+int RemainderOfDivision(a, b);
+{
+if(b / a == 0)
+ {
+    Console.WriteLine("Multiple");
+    return 0;
+ }
+else
+{
+    Console.WriteLine("Not Multiple");
+    
+}
+
+
+}
+
+ Console.WriteLine("Input first number:  ");
+ a = Convert.ToInt32(Console.ReadLine());
+
+ Console.WriteLine("Input second number:  ");
+ b = Convert.ToInt32(Console.ReadLine());
+
+
+ 
