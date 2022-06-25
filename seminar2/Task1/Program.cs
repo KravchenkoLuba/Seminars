7ab4fe282243040538433cd1f7fc425c8110777f
Константин Могутов <kravchenkoluba@yandex.ru>
2022-06-25 13:14:32 +0300

    solve the task

---
+++ seminar2/Task1/Program.cs
@@ -0,0 +1,7 @@
+﻿void ShowSecondDigit(int number);
+{
+    Console.WriteLine("Input three figures ");
+    int number = Convert.ToInt32(Console.ReadLine());
+    int digit = (number%100)/10;
+    Console.WriteLine("Second number is" + digit);
+}
