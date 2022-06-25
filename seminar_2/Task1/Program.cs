5969d9f0026ff310de80ecf3432f7bb50ef52a9e
Константин Могутов <kravchenkoluba@yandex.ru>
2022-06-25 14:30:31 +0300

    solved task1

---
+++ seminar_2/Task1/Program.cs
@@ -0,0 +1,9 @@
+﻿void ShowSecondDigit(int number)
+{
+    int digit = number%100/10;
+    Console.WriteLine("Second number is " + digit);
+}
+
+Console.WriteLine("Input three figures ");
+int number = Convert.ToInt32(Console.ReadLine());
+ShowSecondDigit(number);
