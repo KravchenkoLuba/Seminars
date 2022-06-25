63d5e0d5b75e99dc0052c038ee1bbee3a58a419a
Константин Могутов <kravchenkoluba@yandex.ru>
2022-06-25 16:09:15 +0300

    writte cod

---
+++ seminar_2/Task2/Program.cs
@@ -0,0 +1,22 @@
+﻿void ThirdDigit(int number)
+{
+    if(number>99 && number<1000)
+    res = number%10;
+    Console.WriteLine(res);
+    elif(number>1000 && number<10000);
+    int res1 = number%100/10;
+    Console.WriteLine(res);
+    elif(number>10000 && number<100000);
+    int res2 = number%1000/100;
+    Console.WriteLine(res);
+    elif(number>100000 && number<1000000);
+    int res3 = number%10000/1000;
+    Console.WriteLine(res);
+    
+    
+Console.WriteLine("There is no third digit");
+}
+
+int number = Random.Next(1,1000000);
+Console.WriteLine(number);
+ThirdDigit(number);
