e1603fd8251526b69fa69c48de9de465e86cf6d0
Константин Могутов <kravchenkoluba@yandex.ru>
2022-06-25 18:08:55 +0300

    check work

--- seminar_2/Task2/Program.cs
+++ seminar_2/Task2/Program.cs
@@ -1,22 +1,27 @@
-﻿void ThirdDigit(int number)
+﻿void ThirdDigit()
 {
-    if(number>99 && number<1000)
-    res = number%10;
-    Console.WriteLine(res);
-    elif(number>1000 && number<10000);
-    int res1 = number%100/10;
-    Console.WriteLine(res);
-    elif(number>10000 && number<100000);
-    int res2 = number%1000/100;
-    Console.WriteLine(res);
-    elif(number>100000 && number<1000000);
-    int res3 = number%10000/1000;
-    Console.WriteLine(res);
+    int number = new Random().Next(1,1000000);
+    Console.WriteLine("Our current number is  " + number);
     
+    int res = number % 10;
+    int res1 = number % 100 / 10;
+    int res2 = number % 1000 / 100;
+    int res3 = number % 10000 / 1000;
     
-Console.WriteLine("There is no third digit");
+    if(number < 100)
+       Console.WriteLine("There is no third digit");
+    else   
+        if(number>99 && number<1000) 
+            Console.WriteLine(res);
+    
+        if(number>1000 && number<10000)
+            Console.WriteLine(res1);
+   
+        if(number>10000 && number<100000)
+            Console.WriteLine(res2);
+    
+        if(number>100000 && number<1000000)
+            Console.WriteLine(res3);
 }
 
-int number = Random.Next(1,1000000);
-Console.WriteLine(number);
-ThirdDigit(number);
+ThirdDigit();
