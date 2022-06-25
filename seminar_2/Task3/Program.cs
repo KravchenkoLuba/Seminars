178a4e4a5c6953d75e09935aa45405b3c8cb8c67
Константин Могутов <kravchenkoluba@yandex.ru>
2022-06-25 18:38:36 +0300

    written a solution

---
+++ seminar_2/Task3/Program.cs
@@ -0,0 +1,9 @@
+﻿void Weekday(int number)
+{
+    if(number < 6) Console.WriteLine("No");
+    else Console.WriteLine("Yes");
+}
+
+Console.WriteLine("Input number from 1 to 7 ");
+int number = Convert.ToInt32(Console.ReadLine());
+Weekday(number);
