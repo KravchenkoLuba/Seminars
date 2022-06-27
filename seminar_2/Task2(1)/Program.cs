34572ea898948c90bd68eb08d1d8cddc70f91aa6
Константин Могутов <kravchenkoluba@yandex.ru>
2022-06-28 00:46:01 +0300

    check

---
+++ seminar_2/Task2(1)/Program.cs
@@ -0,0 +1,28 @@
+﻿void ThirdDigit(int number)
+{
+ int res = number%10;
+
+ if(number < 100)
+     Console.WriteLine("There is no third digit");
+      
+ else 
+      if(number<1000)
+      Console.WriteLine(res);
+  
+
+     if(number>999)  
+        while(number>999)
+        {
+          number = number/10;
+          res=number%10;   
+          Console.WriteLine(res);
+        }       
+}
+
+
+Console.WriteLine("Input number ");
+int number = Convert.ToInt32(Console.ReadLine());
+ThirdDigit(number);
+   
+    
+            
