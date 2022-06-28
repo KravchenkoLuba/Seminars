8767c95c02d60a865e3f490c99ff837f8c57f741
Константин Могутов <kravchenkoluba@yandex.ru>
2022-06-28 21:41:45 +0300

    preliminary decision

---
+++ seminar3/Task2/Program.cs
@@ -0,0 +1,12 @@
+﻿double FindDistance(int x1, int y1, int z1,int x2, int y2, int z2)
+{
+    int l1 = x2-x1;
+    int l2 = y2-y1;
+    int l3 = z2-z1;
+    double dis =  Math .Sqrt(l1^2 + l2^2 + l3^2);
+    Console.WriteLine(dis);
+
+}
+
+Console.Write("Input number");
+int N = Convert.ToInt32(Console.ReadLine());
