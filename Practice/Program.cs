42fdc7191446427bebd2eac5a2c3274a05302915
Константин Могутов <kravchenkoluba@yandex.ru>
2022-06-28 20:11:38 +0300

    train

---
+++ Practice/Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+void FindCord (int n)
+{
+    if(n == 1)
+    Console.WriteLine("x>0, y>0");
+    if(n == 2)
+    Console.WriteLine("x<0, y>0");
+    if(n == 3)
+    Console.WriteLine("x<0, y<0");
+    if(n == 4)
+    Console.WriteLine("x>0, y<0");
+    if(n>4 || n<1)
+    Console.WriteLine("Error");
+}
+
+Console.Write("Input Quart number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+FindCord (number);
+*/
+void DoubleNumber (int N)
+{
+    int num = 1;
+    while (num<=N)
+    {
+        Console.Write(num*num+ " " );
+        num++;
+    }
+}
+Console.Write("Input number");
+int N = Convert.ToInt32(Console.ReadLine());
+DoubleNumber (N);
