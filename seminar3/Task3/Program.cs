993b2e5d86101bce0695ae9b76e9e7574476274e
Константин Могутов <kravchenkoluba@yandex.ru>
2022-06-29 20:37:15 +0300

    solved third task

---
+++ seminar3/Task3/Program.cs
@@ -0,0 +1,12 @@
+﻿void CubeNumber (int N)
+{
+    int num = 1;
+    while (num<=N)
+    {
+        Console.Write(num*num*num + " " );
+        num++;
+    }
+}
+Console.Write("Input number ");
+int N = Convert.ToInt32(Console.ReadLine());
+CubeNumber (N);
