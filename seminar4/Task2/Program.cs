e1d3e443f8e0d78c73f4430d85c9b9103ee087e9
Константин Могутов <kravchenkoluba@yandex.ru>
2022-07-03 11:19:10 +0300

    solved second task

---
+++ seminar4/Task2/Program.cs
@@ -0,0 +1,29 @@
+﻿//Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+//1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+//6, 1, 33 -> [6, 1, 33]
+
+int[] CreatArray(int size)
+{
+    int[] array = new int [size];
+
+    for(int i = 0; i<size; i++)
+        array[i] = new Random().Next(0,100);
+        
+    return array;
+}
+
+ void ShowArray(int[] array)
+ {
+    for(int i = 0; i< array.Length; i++)
+        Console.Write(array[i] + " ");
+
+    Console.WriteLine();
+
+ }
+ 
+ Console.Write("Input size array: ");
+ int size = Convert.ToInt32(Console.ReadLine());
+
+ int[] ourArray =  CreatArray(size);
+ ShowArray(ourArray);
+ 
