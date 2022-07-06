a5c1ad441b6953f4b95175adee095ce2d333d546
Константин Могутов <kravchenkoluba@yandex.ru>
2022-07-06 10:26:04 +0300

    solved task 3

---
+++ seminar5/Task3/Program.cs
@@ -0,0 +1,39 @@
+﻿//Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементами массива.
+double[] CreateRealNumberArray(int size)
+{
+    double[] array = new double [size];
+
+    for(int i = 0; i<size; i++)
+        array[i] = new Random().Next(0,100) + new Random().NextDouble();
+        
+    return array;
+}
+
+void ShowArray(double[] array)
+ {
+    for(int i = 0; i< array.Length; i++)
+        Console.Write(array[i] + " ");
+
+    Console.WriteLine();
+
+ }
+
+double FindDiff(double[] array)
+{
+    double minimum = array[0];
+    double maximum = array[0];
+    
+    for(int i = 0; i < array.Length; i++)
+    {   
+        if(array[i] > maximum) maximum = array[i];
+        if(array[i] < minimum) minimum = array[i];
+    }
+    double diff = maximum - minimum;
+    return diff;
+    
+}
+
+double[] RNArray =  CreateRealNumberArray(7);
+ShowArray(RNArray);
+double difference = FindDiff(RNArray);
+Console.WriteLine("The difference between min and max is " + FindDiff(RNArray));
