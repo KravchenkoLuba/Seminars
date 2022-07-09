20976337f191bd552ce25f15e00db6aa748055b4
Константин Могутов <kravchenkoluba@yandex.ru>
2022-07-09 15:18:33 +0300

    solved task1

---
+++ seminar6/Task1/Program.cs
@@ -0,0 +1,19 @@
+﻿ //Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+int NumberOfR(int m)
+{
+    int count = 0;
+    int i = 1;
+    for (i=1; i<=m; i++)
+    {
+        Console.WriteLine("Input a ");
+        int a = Convert.ToInt32(Console.ReadLine());
+        if(a > 0) count++;
+    }
+    return count;
+}
+Console.WriteLine("Input number ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+int count = NumberOfR(m);
+Console.WriteLine("Numbers greater than zero are " + count);
