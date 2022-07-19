691382081b7f9900845764a6e3f53fba005501c3
Константин Могутов <kravchenkoluba@yandex.ru>
2022-07-19 21:53:26 +0300

    solved task1

---
+++ seminar9/Task1/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задайте значение N. Напишите программу, которая найдет факториал числа N рекурсивным методом.
+int Factorial(int n)
+{
+    if(n>0)
+    {
+        return n*Factorial(n-1);
+    } 
+    else return 1;
+}
+
+Console.Write("Input n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(Factorial(n));
